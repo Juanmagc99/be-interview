e99e75b924d28784ec759dbe05ff8085d8c32b1c
Juan Manuel García Criado <juanma101099@gmail.com>
2024-11-05 12:51:45 +0100

    Update organisations.py

--- app/api/routes/organisations.py
+++ app/api/routes/organisations.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from typing import Tuple, List
+from http import HTTPStatus
+from fastapi import APIRouter, HTTPException, Query, status, Depends
 from sqlmodel import select, Session
 
 from app.db import get_db
-from app.models import Location, Organisation, CreateOrganisation
+from app.models import CreateLocation, Location, Organisation, CreateOrganisation
 
 router = APIRouter()
 
@@ -37,16 +39,44 @@ def get_organisation(organisation_id: int, session: Session = Depends(get_db)) -
     return organisation
 
 
-@router.post("/create/locations")
-def create_location():
-    raise NotImplementedError
+@router.post("/create/locations", response_model=Location) 
+def create_location(create_location: CreateLocation,session: Session = Depends(get_db))-> Location:
+    location = Location(
+        organisation_id = create_location.organisation_id,
+        location_name = create_location.location_name,
+        longitude = create_location.longitude,
+        latitude = create_location.latitude,
+    )
+    session.add(location)
+    session.commit()
+    session.refresh(location)
+    return location
 
 
 @router.get("/{organisation_id}/locations")
-def get_organisation_locations(organisation_id: int, session: Session = Depends(get_db)):
-    location_ids = session.exec(select(Location.id).where(Location.organisation_id==organisation_id)).all()
-    result = []
-    for location_id in location_ids:
-        location = session.exec(select(Location).where(Location.id == location_id)).one()
-        result.append({"location_name": location.location_name, "location_longitude": location.longitude, "location_latitude": location.latitude })
-    return result
+def get_organisation_locations(organisation_id: int, 
+session: Session = Depends(get_db), 
+bounding_box: Tuple[float, float, float, float] | None = Query(None, 
+description="min_long, max_long, min_lat, max_lat"),
+example=[-74.25909, -73.70018, 40.4774, 40.9176])->List[Location]:
+    
+    #Check organisation exists
+    org = session.exec(select(Organisation).where(Organisation.id == organisation_id)).first()
+    if not org:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Organisation does not exists")
+    
+    if bounding_box:
+        min_long, max_long, min_lat, max_lat = bounding_box
+        res = session.exec(select(Location)
+                           .filter(Location.organisation_id == organisation_id)
+                           .filter(
+                            Location.longitude >= min_long,
+                            Location.longitude <= max_long,
+                            Location.latitude >= min_lat,
+                            Location.latitude <= max_lat)
+                            )
+        return res.all()
+    else:
+        res = session.exec(select(Location)
+                           .filter(Location.organisation_id == organisation_id))
+        return res.all()
